950711d264f4b53e2b54334b2dc2daa2684d7979
Wasim Raza <wasimraza1698@gmail.com>
2020-12-16 00:13:32 +0530

    Unit Test Added

--- Repository/MedicineStockRepository.cs
+++ Repository/MedicineStockRepository.cs
@@ -11,31 +11,46 @@ namespace MedicineStockModule.Repository
         public static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         private static readonly List<MedicineStockDTO> MedicineStockInformation = new List<MedicineStockDTO>() {
-            new MedicineStockDTO{Name="Medicine1",
-                              ChemicalComposition=new List<string>{"chemical1","chemical2"},
-                              TargetAilment="Orthopedics",
-                              DateOfExpiry=DateTime.Parse("10-10-2021"),
-                              NumberOfTabletsInStock=50},
-            new MedicineStockDTO{Name="Medicine2",
-                              ChemicalComposition=new List<string>{"chemical3","chemical2"},
-                              TargetAilment="General",
-                              DateOfExpiry=DateTime.Parse("10-09-2021"),
-                              NumberOfTabletsInStock=50},
-            new MedicineStockDTO{Name="Medicine3",
-                              ChemicalComposition=new List<string>{"chemical1","chemical2"},
-                              TargetAilment="Gynecology",
-                              DateOfExpiry=DateTime.Parse("10-10-2021"),
-                              NumberOfTabletsInStock=50},
-            new MedicineStockDTO{Name="Medicine4",
-                              ChemicalComposition=new List<string>{"chemical1","chemical2"},
-                              TargetAilment="Orthopedics",
-                              DateOfExpiry=DateTime.Parse("10-01-2021"),
-                              NumberOfTabletsInStock=50},
-            new MedicineStockDTO{Name="Medicine5",
-                              ChemicalComposition=new List<string>{"chemical6","chemical5"},
-                              TargetAilment="Gynecology",
-                              DateOfExpiry=DateTime.Parse("10-10-2021"),
-                              NumberOfTabletsInStock=50},
+            new MedicineStockDTO
+            {
+                Name = "Ibuprofen",
+                ChemicalComposition = new List<string> { "chemical1", "chemical2" },
+                TargetAilment = "Orthopedics",
+                DateOfExpiry = DateTime.Parse("10-10-2021"),
+                NumberOfTabletsInStock = 50
+            },
+            new MedicineStockDTO
+            {
+                Name = "Aceclofenac",
+                ChemicalComposition = new List<string> { "chemical3", "chemical2" },
+                TargetAilment = "General",
+                DateOfExpiry = DateTime.Parse("10-09-2021"),
+                NumberOfTabletsInStock = 50
+            },
+            new MedicineStockDTO
+            {
+                Name = "Ibuprofin",
+                ChemicalComposition = new List<string> { "chemical1", "chemical2" },
+                TargetAilment = "Gynecology",
+                DateOfExpiry = DateTime.Parse("10-10-2021"),
+                NumberOfTabletsInStock = 50
+            },
+            new MedicineStockDTO
+            {
+                Name = "Oxytocin",
+                ChemicalComposition = new List<string> { "chemical1", "chemical2" },
+                TargetAilment = "Gynecology",
+                DateOfExpiry = DateTime.Parse("10-10-2021"),
+                NumberOfTabletsInStock = 50
+            },
+            new MedicineStockDTO
+            {
+                Name = "Paracetamol",
+                ChemicalComposition = new List<string> { "chemical1", "chemical2" },
+                TargetAilment = "General",
+                DateOfExpiry = DateTime.Parse("10-10-2021"),
+                NumberOfTabletsInStock = 50
+            },
 
         };
 
